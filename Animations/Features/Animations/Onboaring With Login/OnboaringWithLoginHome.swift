d8169cf6347397e6a62a7ea77c7e23d060961149
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2023-05-20 10:05:18 +0530

    onboarding documentation added

--- Animations/Features/Animations/Onboaring With Login/OnboaringWithLoginHome.swift
+++ Animations/Features/Animations/Onboaring With Login/OnboaringWithLoginHome.swift
@@ -7,6 +7,14 @@
 
 import SwiftUI
 
+/**
+ The OnboaringWithLoginHome class is responsible for showing onboading screen animation while allowing the user to input login credentials.
+ 
+ - Parameters:
+ - Youtube: https://www.youtube.com/watch?v=9Ztm5ePwY4k&list=WL&index=20
+ - Source Code: https://www.patreon.com/posts/swiftui-app-page-80777273
+**/
+// TODO: Make the onboaring screen
 struct OnboaringWithLoginHome: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
