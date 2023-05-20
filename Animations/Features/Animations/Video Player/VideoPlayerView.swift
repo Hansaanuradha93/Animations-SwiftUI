f2dbfea53e55c6fa031b10806c6facda45a78c43
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2023-05-20 10:25:24 +0530

    Custom video player documentation added

---
+++ Animations/Features/Animations/Video Player/VideoPlayerView.swift
@@ -0,0 +1,29 @@
+//
+//  VideoPlayerView.swift
+//  Animations
+//
+//  Created by Hansa Anuradha on 2023-05-20.
+//
+
+import SwiftUI
+
+/**
+ The VideoPlayerView class is responsible for playing videos in a custom video player.
+ 
+ - Parameters:
+ - Youtube Part 1: https://www.youtube.com/watch?v=BEGPObrbkFU
+ - Youtube Part 2: https://www.youtube.com/watch?v=N4LISLVTB20
+ - Source Code: https://www.patreon.com/posts/swiftui-custom-2-82196310
+**/
+
+struct VideoPlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VideoPlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoPlayerView()
+    }
+}
